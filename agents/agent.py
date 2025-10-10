06cb8d6e8ba5136290c2a211526557f97db49cb0
Eliud2005 <utti232008@utvco.edu.mx>
2025-10-10 08:30:52 -0600

    Aqui ya mueve a cuarentena desde que se crean

--- agents/agent.py
+++ agents/agent.py
@@ -2,6 +2,7 @@ from watchdog.events import FileSystemEventHandler
 import os
 import shutil
 
+#Extensiones que yo agrego para que el sistema detecte como sospechosas
 SUSPICIOUS_EXTENSIONS = ['.exe', '.bat', '.js', '.vbs', '.scr', '.cmd']
 QUARANTINE_FOLDER = "quarantine"
 
@@ -21,7 +22,16 @@ class Agent(FileSystemEventHandler):
         super().__init__()
         self.name = name
         self.queen = queen
-
+#Metodo que mueve archivos sospechosos a cuarentena desde que se crean
+    def on_created(self, event):
+        if not event.is_directory:
+            _, ext = os.path.splitext(event.src_path)
+            if ext.lower() in SUSPICIOUS_EXTENSIONS:
+                print(f"[ALERTA] {self.name} detectó archivo sospechoso creado: {event.src_path}")
+                move_to_quarantine(event.src_path)
+            print(f"{self.name} detectó creación de {event.src_path}")
+            self.queen.report(self.name, event.src_path)
+#Metodo que detecta su hay cambios en otros archivos
     def on_modified(self, event):
         if not event.is_directory:
             _, ext = os.path.splitext(event.src_path)
